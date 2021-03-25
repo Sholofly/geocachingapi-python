1f0bc6ef60dfedb149f2231f949e71e311fd001c
Rudolf Offereins <r.offereins@gmail.com>
2021-03-25 23:27:14 +0000

    Improved test with token handling

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,13 @@
 """Test for Geocaching Api integration."""
 from geocachingapi import GeocachingApi, GeocachingStatus
+#For adding your token add a file call token.py to current folder and add TOKEN = "<your token here>"
+from .token import TOKEN
 import asyncio
 import pytest
 
 @pytest.mark.asyncio
 async def test():
     status:GeocachingStatus = None
-    token = "<insert token here>"
-    api = GeocachingApi(token=token)
+    api = GeocachingApi(token=TOKEN)
     status = await api.update()
-    print(status.user.username)
     assert(status.user.username is not None)
