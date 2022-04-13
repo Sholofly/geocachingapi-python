7daac0054ea34758dcbd2aea20f185bd63945072
Rudolf Offereins <r.offereins@gmail.com>
2022-04-13 21:56:58 +0000

    :broom: Less generic name for token file

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,15 @@
 """Test for Geocaching Api integration."""
-from geocachingapi import GeocachingApi, GeocachingStatus, GeocachingSettings
-#For adding your token add a file call token.py to current folder and add TOKEN = "<your token here>"
-from .token import TOKEN
+from geocachingapi import GeocachingApi, GeocachingStatus
 import asyncio
-import pytest
-
-# @pytest.mark.asyncio
 import logging
+from geocachingapi.models import GeocachingApiEnvironment
 logging.basicConfig(level=logging.DEBUG)
-
+from my_token import TOKEN
 mylogger = logging.getLogger()
 
-@pytest.mark.asyncio
 async def test():
     status:GeocachingStatus = None
-    api = GeocachingApi(token=TOKEN)
+    api = GeocachingApi(token=TOKEN, environment=GeocachingApiEnvironment.Staging)
     status = await api.update()
     print(status.user.reference_code)
     assert(status.user.reference_code is not None)
@@ -23,5 +18,7 @@ async def test():
     assert(False)
 
 
-
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
+loop.close()
 
